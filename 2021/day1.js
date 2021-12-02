75aaa1fab27387b3c577cade2ff4642f05885168
Amanda Araskog <amanda.araskog@kundo.se>
2021-12-02 08:28:26 +0100

    2021 day 1 part 2

--- 2021/day1.js
+++ 2021/day1.js
@@ -1,22 +1,26 @@
 const fs = require('fs');
 
-// 1. How many measurements are larger than the previous measurement?
-const question1 = () => {
+const day1 = () => {
     fs.readFile('./day1.txt', (err, data) => {
         const depths = data.toString();
         const depthsArray = depths.split('\n').map((i) => Number(i));
-        const num = depthsArray.reduce((acc, current, index, array) => {
-            if (current > array[index - 1]){
-             acc ++
+        
+        const sum = depthsArray.reduce((acc, current, i, array) => {
+            if (current > array[i - 1]){
+             acc ++;
             } 
-             return acc
+            return acc;
         }, 0)
-        return (console.log('The number of measurements that are larger than the previous measurement :', num))
+
+        const windowSum = depthsArray.reduce((total, current, i, array) => {
+            if (current + array[i+1] + array[i+2] < array[i+1] + array[i+2] + array[i+3]) {
+                total ++;
+            }
+            return total;
+        }, 0)
+
+        return (console.log('The number of measurements that are larger than the previous measurement :', sum, 'The number of sums larger than the previous sum are: ', windowSum))
     })
 }
 
-question1(); 
-
-// 2. Consider sums of a three-measurement sliding window. 
-//    How many sums are larger than the previous sum?
-
+day1(); 
